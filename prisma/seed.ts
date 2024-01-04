467b8710cf06d5fc8299f338333c353224494df9
mrndi <rooholf@gmail.com>
2024-01-04 18:35:00 +0700

    new seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,75 +6,191 @@ const prisma = new PrismaClient()
 
 try {
   console.log("main")
-  const pw = await Bun.password.hash('password1')
+  const pw1 = await Bun.password.hash('balaputradewa1@')
+  const pw2 = await Bun.password.hash('admin@balaputradewa')
+
 
   // Create Users
   const user1 = await prisma.users.create({
     data: {
-      email: 'user1@example.com',
-      password: pw,
-      name: 'User 1',
+      email: 'susi@balaputradewa.com',
+      password: pw1,
+      name: 'Admin Susi',
       role: 'user',
     },
   });
 
   const user2 = await prisma.users.create({
     data: {
-      email: 'user2@example.com',
-      password: pw,
-      name: 'User 2',
-      role: 'user',
+      email: 'admin@balaputradewa.com',
+      password: pw2,
+      name: 'Super Admin',
+      role: 'admin',
     },
   });
 
   const productSawit = await prisma.products.create({
     data: {
       name: 'Sawit',
-      code: 'PRD1',
+      code: 'SSRC',
     },
   });
 
-  const productKelapa = await prisma.products.create({
+  const productKayu1 = await prisma.products.create({
     data: {
-      name: 'Kelapa',
-      code: 'PRD2',
+      name: 'Kayu',
+      code: 'KCIC',
+    },
+  });
+  const productKayu2 = await prisma.products.create({
+    data: {
+      name: 'Kayu',
+      code: 'KRRC',
     },
   });
 
   const productKaret = await prisma.products.create({
     data: {
       name: 'Karet',
-      code: 'PRD3',
+      code: 'KARC',
     },
   });
 
   //create bank accounts
   const bankAccount1 = await prisma.bankAccounts.create({
     data: {
-      accountName: 'Bank 1',
-      accountNumber: '1234567890',
-      bankName: 'BCA',
-      balance: 1000000,
-
-
+      accountName: 'Bank Mandiri KAS',
+      accountNumber: '1100013668899',
+      bankName: '	Bank Mandiri KAS',
+      balance: 0,
+      bankTransactions: {
+        create: {
+          transactionCode: 'INV101',
+          transactionDate: new Date(),
+          transactionType: 'Debit',
+          amount: 5617702,
+          description: "Setoran Awal"
+        }
+      }
     }
   });
 
   const bankAccount2 = await prisma.bankAccounts.create({
     data: {
-      accountName: 'Bank 2',
-      accountNumber: '0987654321',
-      bankName: 'BNI',
-      balance: 2000000,
-
-
+      accountName: 'Bank Mandiri BPD',
+      accountNumber: '1100016800168',
+      bankName: '	Bank Mandiri BPD',
+      balance: 0,
+      bankTransactions: {
+        create: {
+          transactionCode: 'INV102',
+          transactionDate: new Date(),
+          transactionType: 'Debit',
+          amount: 90400770,
+          description: "Setoran Awal"
+        }
+      }
     }
   });
 
+  const bankAccount3 = await prisma.bankAccounts.create({
+    data: {
+      accountName: 'Bank SINARMAS',
+      accountNumber: '0041338997',
+      bankName: '	Bank SINARMAS',
+      balance: 0,
+      bankTransactions: {
+        create: {
+          transactionCode: 'INV103',
+          transactionDate: new Date(),
+          transactionType: 'Debit',
+          amount: 68436926,
+          description: "Setoran Awal"
+        }
+      }
+    }
+  });
+
+  const bankAccount4 = await prisma.bankAccounts.create({
+    data: {
+      accountName: 'Kas Kecil',
+      accountNumber: '123',
+      bankName: '	Kas Kecil',
+      balance: 0,
+      bankTransactions: {
+        create: {
+          transactionCode: 'INV104',
+          transactionDate: new Date(),
+          transactionType: 'Debit',
+          amount: 777819809,
+          description: "Setoran Awal"
+        }
+      }
+    }
+  });
+
+  const bankAccount5 = await prisma.bankAccounts.create({
+    data: {
+      accountName: 'Dana Rumah',
+      accountNumber: '12345',
+      bankName: '	Dana Rumah',
+      balance: 0,
+      bankTransactions: {
+        create: {
+          transactionCode: 'INV105',
+          transactionDate: new Date(),
+          transactionType: 'Debit',
+          amount: 99111188,
+          description: "Setoran Awal"
+        }
+      }
+    }
+  });
+
+  const firstTransaction = await prisma.bankTransactions.create({
+    data: {
+      transactionCode: 'TRS-2024010205-618',
+      transactionDate: new Date('2024-01-02'),
+      transactionType: 'Transfer',
+      amount: 15000000,
+      description: 'Transfer Ke rekening Dana Rumah 12345',
+      bankAccounts: {
+        connect: {
+          id: bankAccount4.id,
+          balance: bankAccount4.balance - 15000000
+        }
+      }
+    }
+  })
+
+  const secoundTransaction = await prisma.bankTransactions.create({
+    data: {
+      transactionCode: 'TRS-2024010205-82',
+      transactionDate: new Date('2024-01-02'),
+      transactionType: 'Transfer',
+      amount: 15000000,
+      description: 'Transfer Dari rekening Kas Kecil 123',
+      bankAccounts: {
+        connect: {
+          id: bankAccount5.id,
+          balance: bankAccount4.balance + 15000000
+        }
+      }
+    }
+  })
 
 
 
-  //create transaction
+  //create first farmer 
+
+  const farmer1 = await prisma.farmers.create({
+    data: {
+      code: "udinpetani@gmail.com",
+      name: "Udin",
+      phone: '085266069379',
+      address: 'Bayung Lincir'
+    }
+  })
 
 
 
